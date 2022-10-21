217f7200c3becb0162b50675e2887080a28e46d2
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-10-20 20:34:37 -0400

    feat(PalindromIndex): add tests for exercise

---
+++ Exercises/19-PalindromIndex/PalindromIndexTest.php
@@ -0,0 +1,38 @@
+<?php
+
+require('PalindromIndex.php');
+
+use PHPUnit\Framework\TestCase;
+
+class PalindromIndexTest extends TestCase
+{
+    public function testPalindromIndex() : void
+    {
+        $s = "aaab";
+        $expected = 3;
+
+        $result = palindrom_index($s);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testPalindromIndexCase1() : void
+    {
+        $s = "quyjjdcgsvvsgcdjjyq";
+        $expected = 1;
+
+        $result = palindrom_index($s);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testPalindromIndexCase2() : void
+    {
+        $s = "hgygsvlfwcwnswtuhmyaljkqlqjjqlqkjlaymhutwsnwcflvsgygh";
+        $expected = 8;
+
+        $result = palindrom_index($s);
+
+        $this->assertEquals($expected, $result);
+    }
+}
