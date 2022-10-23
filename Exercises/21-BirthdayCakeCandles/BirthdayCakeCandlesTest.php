4960e0b9e330386f95635990ef808f01294acf20
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-10-22 20:20:21 -0400

    feat(BirthdayCakeCandles): add tests for exercise

---
+++ Exercises/21-BirthdayCakeCandles/BirthdayCakeCandlesTest.php
@@ -0,0 +1,29 @@
+<?php
+require('BirthdayCakeCandles.php');
+
+use PHPUnit\Framework\TestCase;
+
+class birthdayCakeCandlesTest extends TestCase
+{
+    public function testStaircase()
+    {
+        $candles = [4,4,1,3];
+        $expected = 2;
+
+        $result = birthdayCakeCandles($candles);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testStaircaseCase1()
+    {
+        $candles = [3,2,1,3];
+        $expected = 2;
+
+        $result = birthdayCakeCandles($candles);
+
+        $this->assertEquals($expected, $result);
+    }
+}
+
+?>
